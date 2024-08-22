16a07aa438139b2ce255dc7514501fdae0cefd40
‘mido1345’ <mido.cla.v3v@gmail.com>
2024-08-22 20:41:01 +0900

    課題訂正

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -13,12 +13,17 @@
              <th>ID</th>
              <th>タイトル</th>
              <th>本文</th>
+             <th>作成日時</th>
+             <th>更新日時</th>
+
          </tr>
          @foreach($posts as $post)
              <tr>
                  <td>{{ $post->id }}</td>
                  <td>{{ $post->title }}</td>
                  <td>{{ $post->content }}</td>
+                 <td>{{ $post->created_at }}</td>
+                 <td>{{ $post->updated_at }}</td>
              </tr>            
          @endforeach       
      </table>
